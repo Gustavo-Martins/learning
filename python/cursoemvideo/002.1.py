85029319e63bbb46f9cce934563cb18b6fbebe48
Gustavo Martins <martins.gustavo@protonmail.com>
2020-08-05 10:58:31 -0300

    Adds exercises 002 and 002.1, input and printing

---
+++ python/cursoemvideo/002.1.py
@@ -0,0 +1,2 @@
+name = input("Digite o seu nome: ")
+print("É um prazer te conhecer, {}!".format(name))
---
+++ python/cursoemvideo/002.py
@@ -0,0 +1,3 @@
+name = input("Digite o seu nome: ")
+reply = ("É um prazer te conhecer, %s." %name)
+print(reply)
