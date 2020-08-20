f945c1fae50d28530e647044f4397f71bec4c355
Gustavo Martins <martins.gustavo@protonmail.com>
2020-08-20 11:03:25 -0300

    Adds python string formatting docs

--- python/cursoemvideo/002.1.py
+++ python/cursoemvideo/002.1.py
@@ -1,2 +1,4 @@
+# https://www.python.org/dev/peps/pep-3101/
+
 name = input("Digite o seu nome: ")
 print("É um prazer te conhecer, {}!".format(name))
--- python/cursoemvideo/002.py
+++ python/cursoemvideo/002.py
@@ -1,3 +1,5 @@
+# https://docs.python.org/3/library/string.html#string-formatting
+
 name = input("Digite o seu nome: ")
 reply = ("É um prazer te conhecer, %s." %name)
 print(reply)
