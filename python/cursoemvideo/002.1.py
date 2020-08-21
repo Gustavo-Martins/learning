06bc7c15f5a7b25bdc685353fd1da30b8793fc99
Gustavo Martins <martins.gustavo@protonmail.com>
2020-08-21 11:22:51 -0300

    Code linting

--- python/cursoemvideo/002.1.py
+++ python/cursoemvideo/002.1.py
@@ -1,4 +1,4 @@
 # https://www.python.org/dev/peps/pep-3101/
 
-name = input("Digite o seu nome: ")
-print("É um prazer te conhecer, {}!".format(name))
+name = input('Digite o seu nome: ')
+print('É um prazer te conhecer, {}!'.format(name))
