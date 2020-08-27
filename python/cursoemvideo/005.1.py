8563b5b354033201ba9068794113b30d63d4b8e0
Gustavo Martins <martins.gustavo@protonmail.com>
2020-08-27 10:48:27 -0300

    Adds previous and next number printing

---
+++ python/cursoemvideo/005.1.py
@@ -0,0 +1,5 @@
+# Gets input and prints previous and next number
+
+number = int(input('Digite um número inteiro: '))
+
+print('O seu número digitado é {}, o anterior é {}, e o próximo é {}'.format(number, (number-1), (number+1)))
---
+++ python/cursoemvideo/005.py
@@ -0,0 +1,7 @@
+# Gets input and prints previous and next number
+
+number = int(input('Digite um número inteiro: '))
+previous_number = number - 1
+next_number = number + 1
+
+print('O seu número digitado é {}, o anterior é {}, e o próximo é {}.'.format(number, previous_number, next_number))
