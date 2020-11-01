7edd3820c7540b26fd603131a7b62d57ce04dfc7
Gustavo Martins <martins.gustavo@protonmail.com>
2020-11-01 15:49:21 -0300

    Adds discounted price calculator

---
+++ python/cursoemvideo/012.1.py
@@ -0,0 +1,6 @@
+# Reads float and prints price with discount
+
+price = float(input('Digite o preço do produto: R$ '))
+new_price = price - (price * 5 / 100)
+
+print('O preço do produto é R$ {:.2f}, com desconto de 5%, custará R$ {}.'.format(price, new_price))
---
+++ python/cursoemvideo/012.py
@@ -0,0 +1,7 @@
+# Reads float and prints price with discount
+
+price = float(input('Digite o preço do produto: '))
+discount = (price * 5) / 100
+new_price = price - discount
+
+print('O produto custa R$ {:.2f}, com desconto de 5%, o produto custará: R$ {:.2f}.'.format(price, new_price))
