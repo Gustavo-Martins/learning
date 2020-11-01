ae2e7df37649fed7304922b2e91957e1f78df53b
Gustavo Martins <martins.gustavo@protonmail.com>
2020-11-01 15:06:28 -0300

    Adds money conversion

---
+++ python/cursoemvideo/010.2.py
@@ -0,0 +1,7 @@
+# Reads float and prints BRL money conversion to JPY
+# In 2020/11/01 JP¥ 1.00 = R$ 0.060  :)
+
+brl = float(input('Por favor, digite quanto dinheiro você tem na carteira: R$ '))
+jpy = brl / 0.060
+
+print('Com R$ {:.2f} você pode comprar ¥ {:.2f}.'.format(brl, jpy))
