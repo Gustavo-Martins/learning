1cc72ca257b7d775cdcbfc3ec6cd3cfcb54b4404
Gustavo Martins <martins.gustavo@protonmail.com>
2020-11-03 09:33:03 -0300

    Adds raised salary calculator

---
+++ python/cursoemvideo/013.1.py
@@ -0,0 +1,5 @@
+# Reads salary float value and prints value with 15% raise
+
+salary = float(input('Digite o valor do seu salário: R$ '))
+
+print('O seu salário foi aumentado de R$ {:.2f} para R$ {:.2f}.'.format(salary, salary + salary * 0.15))
---
+++ python/cursoemvideo/013.py
@@ -0,0 +1,6 @@
+# Reads salary float value and prints value with 15% raise
+
+salary = float(input('Digite o valor do seu salário: R$ '))
+new_salary = salary + (salary * 0.15)
+
+print('O seu salário foi aumentado de R$ {:.2f} para R$ {:.2f}.'.format(salary, new_salary))
