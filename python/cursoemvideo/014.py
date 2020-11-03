a5e99a17ae2abed3b81253113cdaafb14fb3dcd6
Gustavo Martins <martins.gustavo@protonmail.com>
2020-11-03 09:49:45 -0300

    Adds Celsius to Fahrenheit converter

---
+++ python/cursoemvideo/014.py
@@ -0,0 +1,6 @@
+# Converts Celsius to Fahrenheit
+
+celsius = float(input('Digite a temperatura (°C): '))
+fahrenheit = celsius / 5 * 9 + 32
+
+print('{:.1f} °C equivalem a {:.1f} °F.'.format(celsius, fahrenheit))
