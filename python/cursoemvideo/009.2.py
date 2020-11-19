e089389d39ddaca9a1cec6b97de0005f86afaa1e
Gustavo Martins <martins.gustavo@protonmail.com>
2020-11-19 10:38:53 -0300

    Adds random name picker

---
+++ python/cursoemvideo/009.2.py
@@ -0,0 +1,25 @@
+# Reads integer and prints basic math operations
+
+def inputInteger(number):
+    while True:
+        try:
+            number = int(input(number))
+        except:
+            print('Por favor, digite um número inteiro.')
+        else:
+            return number
+
+
+numero = inputInteger('Digite um número inteiro para ver a sua tabuada: ')
+
+def basicMath():
+    i = 1
+    inputInteger(numero)
+    print('Número recebido {}'.format(numero))
+    for n in range(1, 11):
+        print('Teste')
+        print('{}'.format(numero * i))
+        i += 1
+
+# print('{}'.format(numero))
+basicMath()
