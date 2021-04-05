6df8c6de850a6e49f50f021cb0b1c0a4e134d64d
Gustavo Martins <martins.gustavo@protonmail.com>
2021-04-05 10:14:11 -0300

    Adds name analyzer

---
+++ python/cursoemvideo/022.py
@@ -0,0 +1,12 @@
+# Reads name and prints info
+
+name = str(input('Digite o seu nome completo: '))
+nospace = name.replace(' ', '')
+div = name.split()
+
+print('O seu nome em maiúsculas é: {}'.format(name.lower()))
+print('O seu nome em minúsculas é: {}'.format(name.upper()))
+print('O seu nome sem espaços é: {}'.format(nospace))
+print('Número de caracteres sem espaço: {}'.format(len(nospace)))
+print('O seu primeiro nome é: {}'.format(div[0]))
+print('O seu primeiro nome tem {} letras.'.format(len(div[0])))
