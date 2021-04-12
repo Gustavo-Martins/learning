9b5a1c7275d3278a6b64f5d04bcdeb341ac00293
Gustavo Martins <martins.gustavo@protonmail.com>
2021-04-12 10:16:15 -0300

    Adds number analyzer

---
+++ python/cursoemvideo/023.py
@@ -0,0 +1,13 @@
+# Reads number and outputs digits
+
+number = int(input('Digite um numero de 0 a 9999: '))
+u = number // 1 % 10
+d = number // 10 % 10
+c = number // 100 % 10
+m = number // 1000 % 10
+
+print('O número {} contém: '.format(number))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
