17cf802f0490c73ca241775352dfc3cd4dc233bc
Gustavo Martins <martins.gustavo@protonmail.com>
2021-04-13 17:09:01 -0300

    Adds letter A search script

---
+++ python/cursoemvideo/026.py
@@ -0,0 +1,8 @@
+# Gets input and prints 'A' occurences and position
+
+text = str(input('Digite o texto: ')).lower().strip()
+
+print(text)
+print('A letra "A" aparece {} vezes no texto.'.format(text.count('a')))
+print('A letra "A" aparece pela primeira vez na posição {}.'.format(text.find('a')+1))
+print('A letra "A" aparece pela última vez na posição {}.'.format(text.rfind('a')+1))
