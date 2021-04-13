92e313e7a25c19a63524438fb0eec04a0d485cbe
Gustavo Martins <martins.gustavo@protonmail.com>
2021-04-13 17:31:34 -0300

    Adds first and last name printing

---
+++ python/cursoemvideo/027.py
@@ -0,0 +1,8 @@
+# Reads input and prints first and last name
+
+name = str(input('Digite o seu nome completo: ')).split()
+
+print('Prazer em conhecê-lo!')
+print(name)
+print('O seu primeiro nome é: {}'.format(name[0]))
+print(' O seu último nome é: {}'.format(name[-1]))
