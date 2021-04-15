098547c71f8dff52973860f1368facec47b8e227
Gustavo Martins <martins.gustavo@protonmail.com>
2021-04-15 15:23:55 -0300

    Adds leap year classifier

---
+++ python/cursoemvideo/032.py
@@ -0,0 +1,11 @@
+# Prints if a date is a leap year
+from datetime import date
+
+
+year = int(input('Qual ano deseja analisar? Digite 0 para o ano atual: '))
+if year == 0:
+    year = date.today().year
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print('O ano {} é BISSEXTO.'.format(year))
+else:
+    print('O ano {} NÃO É BISSEXTO.'.format(year))
