6eb3732d334f3c4e46ca6d263c965e459ca9ce85
Gustavo Martins <martins.gustavo@protonmail.com>
2021-04-15 11:44:09 -0300

    Adds speeding fine calculator

---
+++ python/cursoemvideo/029.1.py
@@ -0,0 +1,8 @@
+# Reads speed and prints if driver gets a ticket
+
+velocidade = float(input('Qual é a velocidade atual do carro? '))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite de velocidade que é de 80Km/h')
+    multa = (velocidade - 80) * 7
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+print('Tenha um bom dia! Dirija com segurança!')
