ce757492ea69a2ee8e53e228b7980451ccbf2f98
Gustavo Martins <martins.gustavo@protonmail.com>
2021-05-04 15:51:14 -0300

    Adds Military Conscription checker

---
+++ python/cursoemvideo/039.1.py
@@ -0,0 +1,18 @@
+# Military conscription checker
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento.'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {}.'.format(ano))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você já deveria ter se alistado há {} anos.'.format(saldo))
+    ano = atual - saldo
+    print('O seu alistamento foi em {}.'.format(ano))
---
+++ python/cursoemvideo/039.py
@@ -0,0 +1,13 @@
+# Military conscription checker
+from datetime import date
+birth_year = int(input('Por favor, digite o seu ano de nascimento: '))
+current_year = date.today().year
+age = current_year - birth_year
+
+if age < 18:
+    print('Jovem, ainda faltam {} anos para o seu ano de alistamento.'.format(18 - age))
+elif age > 18:
+    print('Jovem, o Brasil te aguarda há {} anos pelo seu alistamento.'.format(age - 18))
+else:
+    print('''Jovem, está no seu ano de servir ao Brasil,
+    apresente-se no posto de alistamento mais próximo.''')
