ebbf25dd9a07bd10d783743674bf1270b9d904ad
Gustavo Martins <martins.gustavo@protonmail.com>
2021-05-04 15:14:03 -0300

    Adds number comparison

---
+++ python/cursoemvideo/038.py
@@ -0,0 +1,10 @@
+# Compares 2 numbers
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+
+if a > b:
+    print('O primeiro valor é maior.')
+elif a < b:
+    print('O segundo valor é menor.')
+else:
+    print('Não existe valor maior, os dois são iguais.')
