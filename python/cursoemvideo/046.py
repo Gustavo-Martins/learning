612816af523953e047889eca39725249e8488d87
Gustavo Martins <martins.gustavo@protonmail.com>
2021-05-11 15:23:37 -0300

    Adds countdown

---
+++ python/cursoemvideo/046.py
@@ -0,0 +1,11 @@
+# Countdown
+from time import sleep
+
+
+print('T Minus 20 seconds and counting.')
+sleep(10)
+for c in range (10, -1, -1):
+    print(c)
+    sleep(1)
+print('BLASTOFF!')
+print('Burn, baby. BURN!')
