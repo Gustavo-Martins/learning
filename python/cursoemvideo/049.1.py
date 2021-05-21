9db3313747bf57edc8b701eeaf697801ea26c5a3
Gustavo Martins <martins.gustavo@protonmail.com>
2021-05-21 09:29:59 -0300

    Adds basic math operation

---
+++ python/cursoemvideo/049.1.py
@@ -0,0 +1,8 @@
+# Reads number and inputs basic math operation
+number = int(input('Digite um número para ver a sua tabuada: '))
+
+def basicMath():
+    for c in range (1, 11):
+        print('{} x {} = {}'.format(number, c, (number * c)))
+
+basicMath()
---
+++ python/cursoemvideo/049.py
@@ -0,0 +1,5 @@
+# Reads number and prints basic math operations
+number = int(input('Digite um número para ver sua tabuada: '))
+for c in range (1, 11):
+    print('{} x {} = {}'.format(number, c, (number * c)))
+    
