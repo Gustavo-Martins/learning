044c4748b9d441569d8987eeb044c1213450d457
Gustavo Martins <martins.gustavo@protonmail.com>
2021-05-24 15:53:40 -0300

    Adds prime number classification

---
+++ python/cursoemvideo/052.py
@@ -0,0 +1,17 @@
+# Reads number and prints if it's a prime 'number
+number = int(input('Digite um número: '))
+divisibles = 0
+
+for c in range (1, number + 1):
+    if number % c == 0:
+        print('\033[33m', end = '')
+        divisibles += 1   
+    else:
+        print('\033[31m', end = '')
+    print('{}'.format(c), end = ' ')
+
+print('\n\033[mO número {} foi divisível {} vezes.'.format(number, divisibles))
+if divisibles > 2:
+    print('O número {} não é primo.'.format(number))
+else:
+    print('O número {} é primo.'.format(number))
