f50827882d129fc145e397caf92296c854db9661
Gustavo Martins <martins.gustavo@protonmail.com>
2021-05-26 15:55:56 -0300

    Adds maximum and minimum values classification

---
+++ python/cursoemvideo/055.py
@@ -0,0 +1,16 @@
+# Reads 5 person weights and prints maximum and minimum values
+minimum = 0
+maximum = 0
+
+for person in range (1, 6):
+    weight = float(input('Digite o peso da {}ª pessoa: '.format(person)))
+    if person == 1:
+        minimum = person
+        maximum = person
+    else:
+        if weight > maximum:
+            maximum = weight
+        if weight < minimum:
+            minimum = weight
+    
+print('O maior peso lido é {} Kg, e o menor peso lido é {} Kg.'.format(maximum, minimum))
