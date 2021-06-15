6431d8c7172fa4f802a698b942ce52276aab1b58
Gustavo Martins <martins.gustavo@protonmail.com>
2021-06-15 14:33:55 -0300

    Adds gender input check

---
+++ python/cursoemvideo/057.py
@@ -0,0 +1,5 @@
+# Checks gender input
+gender = str(input('Informe o seu gênero: [M/F] ').strip().upper()[0])
+while gender not in 'MF':
+	gender = str(input('Dados inválidos. Por favor, informe o seu gênero: ').strip().upper([0]))
+print('Gênero {} registrado com sucesso.'.format(gender))
