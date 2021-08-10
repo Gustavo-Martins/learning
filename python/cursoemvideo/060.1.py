4f6ba7c53eb41a2e4003e4682ea7a6a44fb36aa2
Gustavo Martins <martins.gustavo@protonmail.com>
2021-08-10 10:59:17 -0300

    Adds factorial calculator

---
+++ python/cursoemvideo/060.1.py
@@ -0,0 +1,13 @@
+# Factorial calculator
+
+n = int(input('Digite um número para calcular seu fatorial: '))
+counter = n
+f = 1 # Prevents multiplication by zero
+
+print('Calculando {}! = '.format(n), end='')
+while counter > 0:
+	print('{}'.format(counter), end='')
+	print(' x ' if counter > 1 else ' = ', end='')
+	f *= counter
+	counter -= 1
+print('{}'.format(f))
---
+++ python/cursoemvideo/060.py
@@ -0,0 +1,6 @@
+# Factorial calculator
+from math import factorial
+
+
+n = int(input('Digite um número: '))
+print('{}! = {} '.format(n, factorial(n)))
