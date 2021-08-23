f1b8662d3fb5d58c3bd912a6367271d922b8e6a3
Gustavo Martins <martins.gustavo@protonmail.com>
2021-08-23 15:58:54 -0300

    Adds numbers and prints sum

---
+++ python/cursoemvideo/064.1.py
@@ -0,0 +1,8 @@
+# Reads numbers and inputs sum
+n = count = sum = 0
+n = int(input('Digite um número [999 para parar]: '))
+while n != 999:
+	sum += n
+	count += 1
+	n = int(input('Digite um número [999 para parar]: '))
+print('Você digitou {} números e a soma entre eles foi {}.'.format(count, sum))
---
+++ python/cursoemvideo/064.py
@@ -0,0 +1,11 @@
+# Reads numbers and prints sum
+n = 0
+sum = 0
+numbers = 0
+
+while n != 999:
+	n = int(input('Digite um número [999 para parar]: '))
+	if n != 999:
+		sum += n
+		numbers += 1
+print('Você digitou {} números e a soma entre eles foi {}.'.format(numbers, sum))
