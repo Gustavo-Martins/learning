3da6f153f3e8d350e2522e4d936141138ed7d979
Gustavo Martins <martins.gustavo@protonmail.com>
2021-08-24 14:14:18 -0300

    Adds improved sum Introduces the use of 'break'

---
+++ python/cursoemvideo/066.py
@@ -0,0 +1,9 @@
+# Prints sum
+count = sum = 0
+while True:
+	n = int(input('Digite um valor (999 para parar): '))
+	if n == 999:
+		break
+	count += 1
+	sum += n
+print(f'A soma dos {count} valores foi {sum}!')
