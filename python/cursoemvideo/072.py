5a9c45ce9f8164110777aea92877e78aa69801d8
Gustavo Martins <martins.gustavo@protonmail.com>
2021-08-27 09:32:43 -0300

    Adds numerals printing

---
+++ python/cursoemvideo/072.py
@@ -0,0 +1,12 @@
+# Prints numerals
+numerals = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 
+'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze','treze', 
+'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 
+'dezenove', 'vinte')
+
+while True:
+	n = int(input('Digite um número entre 0 e 20: '))
+	if n in range (0, 21):
+		break
+	print('Tente novamente. ', end='')
+print(f'Você digitou o número: {numerals[n]}')
