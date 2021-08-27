f6bb3825b6786f8cb919d2574b629a6db37ed709
Gustavo Martins <martins.gustavo@protonmail.com>
2021-08-27 10:55:19 -0300

    Adds random numbers sorting and printing

---
+++ python/cursoemvideo/074.py
@@ -0,0 +1,9 @@
+# Randon numbers printing
+from random import randint
+
+numbers = (randint(0, 99), randint(0, 99), randint(0, 99), 
+randint(0, 99), randint(0, 99))
+
+print(f'Os valores sorteados foram: {numbers}')
+print(f'Maior valor: {max(numbers)}')
+print(f'Menor valor: {min(numbers)}')
