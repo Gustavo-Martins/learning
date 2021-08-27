77d8f395aaf0c400aa0efb6e62502151d83f03c5
Gustavo Martins <martins.gustavo@protonmail.com>
2021-08-27 09:58:22 -0300

    Adds Tokyo Olympic Games NOCs classification

---
+++ python/cursoemvideo/073.py
@@ -0,0 +1,11 @@
+# Sorts National Olympics Committees
+nocs_table = ('United States', 'China', 'Japan', 'Great Britain', 'Russia', 
+'Australia', 'The Netherlands', 'France', 'Germany', 'Italy', 'Canada', 
+'Brazil', 'New Zealand', 'Cuba', 'Hungary', 'South Korea', 'Poland', 
+'Czech Republic', 'Kenya', 'Norway')
+
+
+print(f'Five best NOCs: {nocs_table[0:6]}')
+print(f'16 to 20th NOCs: {nocs_table[-4:]}')
+print(f'NOCs in alphabetical order: {sorted(nocs_table)}')
+print('Brazil result: {}'.format({nocs_table.index('Brazil')}))
