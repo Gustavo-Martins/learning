b57ae005e6bb0fa41aca988e524e14e6ecdeb9ff
Gustavo Martins <martins.gustavo@protonmail.com>
2021-09-16 14:34:03 -0300

    Adds vowels printing

---
+++ python/cursoemvideo/077.py
@@ -0,0 +1,13 @@
+# Reads word list and print vowels
+words = ('APRENDER', 'PROGRAMAR',
+	'LINGUAGEM', 'PYTHON',
+	'CURSO', 'GRATIS',
+	'ESTUDAR', 'PRATICAR',
+	'TRABALHAR', 'MERCADO',
+	'PROGRAMADOR', 'FUTURO')
+
+for pos in words:
+	print(f'\nNa palavra {pos} temos ', end='')
+	for letter in pos:
+		if letter in 'AEIOU':
+				print(letter.lower(), end=' ')
