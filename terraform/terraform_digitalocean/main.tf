1790cf01034e91fc091d7a73fdc97ed6fe627427
Gustavo Martins <martins.gustavo@protonmail.com>
2021-10-15 16:23:22 +0200

    Init Terraform Digital Ocean project

---
+++ terraform/terraform_digitalocean/main.tf
@@ -0,0 +1,10 @@
+provider "digitalocean" {}
+
+terraform {
+    required_providers{
+        digitalocean = {
+            source = "digitalocean/digitalocean"
+            version = "~> 2.0"
+        }
+    }
+}
