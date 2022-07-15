fa5f3e1c0dfd75bd425300e555454ff16e163099
Gustavo Martins <martins.gustavo@protonmail.com>
2022-07-15 10:54:54 +0200

    Adds age average to people registry

--- python/cursoemvideo/094.py
+++ python/cursoemvideo/094.py
@@ -1,7 +1,9 @@
+# People registry and age average
 person = {}
 registry = []
 choice = ' '
-
+age_sum = 0
+average_age = 0
 while True:
 	person['name'] = str(input('Nome: '))
 	person['sex'] = str(input('Sexo: [M/F] ').strip().upper()[0])
@@ -9,12 +11,12 @@ while True:
 		print('ERRO! Por favor, digite apenas M ou F.')
 		person['sex'] = str(input('Sexo: [M/F] ').strip().upper()[0])
 	person['age'] = int(input('Idade: '))
+	age_sum += person['age']
 	choice = str(input('Quer continuar? [S/N] ').strip().upper()[0])
 	while choice not in 'SN':
 		print('ERRO! Responda apenas S ou N.')
 		choice = str(input('Quer continuar? [S/N] ').strip().upper()[0])
 	registry.append(person.copy())
-	print(person)
 	person.clear()
 	if choice in 'S':
 		person['name'] = str(input('Nome: '))
@@ -23,7 +25,7 @@ while True:
 			print('ERRO! Por favor, digite apenas M ou F.')
 			person['sex'] = str(input('Sexo: [M/F] ').strip().upper()[0])
 		person['age'] = int(input('Idade: '))
-		print(person)
+		age_sum += person['age']
 		registry.append(person.copy())
 		choice = str(input('Quer continuar? [S/N] ').strip().upper()[0])
 		while choice not in 'SN':
@@ -31,4 +33,16 @@ while True:
 			choice = str(input('Quer continuar? [S/N] ').strip().upper()[0])
 	if choice in 'N':
 		break
-print(registry)
+print('-=' * 30)
+print(f'Ao todo foram cadastradas {len(registry)} pessoas.')
+average_age = age_sum / len(registry)
+print(f'A média de idade é de {average_age} anos.')
+print('As mulheres cadastradas foram ', end='')
+for person in registry:
+	if person['sex'] in 'F':
+		print(f'{person["name"]}', end=', ')
+print()
+print('Lista das pessoas com idade acima da média: ')
+for person in registry:
+	if person['age'] > average_age:
+		print(f'{person["name"]}', end=', ')
