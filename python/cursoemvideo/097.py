942c1d5d5e898c2c6aac111a549de1ebe5e114d8
Gustavo Martins <martins.gustavo@protonmail.com>
2022-07-27 10:03:41 +0200

    Adds formated text printing

---
+++ python/cursoemvideo/097.py
@@ -0,0 +1,11 @@
+# Prints text
+def write(txt):
+	length = len(txt) + 4
+	print('-' * length)
+	print(f'  {txt}')
+	print('-' * length)
+
+
+write('Gustavo Guanabara')
+write('Curso de Python no YouTube')
+write('CeV')
