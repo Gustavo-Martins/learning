51166e752a3d87bba370a58bdfe762717e604bf8
Gustavo Martins <martins.gustavo@protonmail.com>
2022-08-04 11:01:04 +0200

    Adds random numbers even sum

---
+++ python/cursoemvideo/100.py
@@ -0,0 +1,27 @@
+# Draw numbers and sum evens
+from random import randint
+from time import sleep
+
+
+
+def draw_num(list):
+	print('Sorteando 5 valores da lista: ', end='')
+	for i in range (0, 5):
+		n = randint(1, 10)
+		list.append(n)
+		print(f'{n} ', end='', flush=True)
+		sleep(0.3)
+	print('PRONTO!')
+
+
+def even_sum(list):
+	even_total = 0
+	for n in list:
+		if n % 2 == 0:
+			even_total += n
+	print(f'Somando os valores pares de {list}, temos {even_total}.')
+
+
+numbers = []
+draw_num(numbers)
+even_sum(numbers)
