64e49640060c7601bd9cc7dadb2054c5025886bd
Gustavo Martins <martins.gustavo@protonmail.com>
2022-08-09 15:34:05 +0200

    Adds sh while loop with cases

---
+++ shell_script/shell_scripting_tutorial/10.while3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+while read input_text
+do
+	case $input_text in
+		hello )		echo English	;;
+		howdy )		echo American	;;
+		gday )		echo Australian	;;
+		bonjour )	echo French	;;
+		"guten tag" )	echo German	;;
+		* )		echo Unkown Language: $input_text
+			;;
+	esac
+done < myfile.txt
+
