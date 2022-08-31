6e89106aaac51af2ed0c1cf5c5ceba3d35295e14
Gustavo Martins <martins.gustavo@protonmail.com>
2022-08-31 11:59:40 +0200

    Adds extra scripts to cursoemvideo

---
+++ python/cursoemvideo/extras/problema.py
@@ -0,0 +1,66 @@
+list = []
+
+
+def add_number(numero):
+	list.append(numero)
+
+
+def remove_queue():
+	list.pop(0)
+
+
+def remove_stack():
+	list.pop()
+
+
+def print_list():
+	print(list)
+
+
+while True:
+	option = str(input('Qual estrutura deseja utilizar (Pilha, Fila ou Sair)? ').lower())
+	if option == 'pilha':
+		print('Você escolheu Pilha.')
+		while True:
+			sel_function = str(input('Adicionar número, Remover número, ou Sair? ').lower())
+			if sel_function == 'adicionar':
+				numero = int(input('Digite o número a ser adicionado: '))
+				add_number(numero)
+				print_list()
+			elif sel_function == 'remover':
+				if len(list) > 0:
+					remove_stack()
+					print_list()
+				else:
+					print('Não é possível remover números de uma pilha vazia.')
+			elif sel_function == 'sair':
+				print('Retornando ao menu principal...')
+				list.clear()
+				break
+			else:
+				print('Por favor, digite uma opção válida.')
+	elif option == 'fila':
+		print('Você escolheu Fila.')
+		while True:
+			sel_function = str(input('Adicionar número, Remover número, ou Sair? ').lower())
+			if sel_function == 'adicionar':
+				numero = int(input('Digite o número a ser adicionado: '))
+				add_number(numero)
+				print_list()
+			elif sel_function == 'remover':
+				if len(list) > 0:
+					remove_queue()
+					print_list()
+				else:
+					print('Não é possível remover números de uma fila vazia.')
+			elif sel_function == 'sair':
+				print('Retornando ao menu principal...')
+				list.clear()
+				break
+			else:
+				print('Por favor, digite uma opção válida.')
+	elif option == 'sair':
+		print('Encerrando programa...')
+		break
+	else:
+		print('Por favor, selecione uma opção válida.')
---
+++ python/cursoemvideo/extras/problema2.py
@@ -0,0 +1,68 @@
+lista = []
+
+def Adicionar(numero):
+	lista.append(numero)
+
+
+def RemoverPilha():
+	lista.pop()
+
+
+def RemoverFila():
+	lista.pop(0)
+
+
+def MostraLista():
+	print(lista)
+
+
+while True:
+	seleciona = str(input('Digite uma opção: Pilha, Fila, Sair: ').strip().lower()[0])
+	print(seleciona)
+	if seleciona == 'p':
+		print('Selecionou Pilha')
+		while True:
+			sel_funcao = str(input('Digite Adicionar, Remover, Sair: ').strip().lower()[0])
+			if sel_funcao == 'a':
+				print('Selecionou Adicionar')
+				numero = int(input('Digite o número a ser adicionado: '))
+				Adicionar(numero)
+				MostraLista()
+			elif sel_funcao == 'r':
+				if len(lista) > 0:
+					print('Selecionou Remover')
+					RemoverPilha()
+					MostraLista()
+				else:
+					print('Não é possível remover um número de uma pilha vazia.')
+			elif sel_funcao == 's':
+				print('Retornando ao menu principal...')
+				break
+			else:
+				print('Por favor, digite uma opção válida.')
+	elif seleciona == 'f':
+		print('Selecionou Fila')
+		while True:
+			sel_funcao = str(input('Digite Adicionar, Remover, Sair: ').strip().lower()[0])
+			if sel_funcao == 'a':
+				print('Selecionou Adicionar')
+				numero = int(input('Digite o número a ser adicionado: '))
+				Adicionar(numero)
+				MostraLista()
+			elif sel_funcao == 'r':
+				print('Selecionou Remover')
+				if len(lista) > 0:
+					RemoverFila()
+					MostraLista()
+				else:
+					print('Não é possível remover um número de uma fila vazia.')
+			elif sel_funcao == 's':
+				print('Retornando ao menu principal...')
+				break
+			else:
+				print('Por favor, digite uma opção válida.')	
+	elif seleciona == 's':
+		print('Encerrando o programa...')
+		break
+	else:
+		print('Por favor, digite uma opção válida.')
