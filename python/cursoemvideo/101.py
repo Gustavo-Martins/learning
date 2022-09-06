ef0e51af6a4caece9e3956666b5fae18b3314490
Gustavo Martins <martins.gustavo@protonmail.com>
2022-09-06 13:18:08 +0200

    Adds voting rights age verification

---
+++ python/cursoemvideo/101.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+
+def vote(year=0):
+	"""Checks voting rights by age, returns classification.
+
+	Keyword arguments:
+	year -- default == 0
+	"""
+	age = date.today().year - year
+	if age < 16:
+		return f'Com {age} anos: Não Vota'
+	elif 16 <= age < 18 or age > 70:
+		return f'Com {age} anos: Voto Facultativo'
+	else:
+		return f'Com {age} anos: Voto Obrigatório'
+
+
+print('-' * 30)
+year = int(input('Em que ano você nasceu? '))
+print(vote(year))
