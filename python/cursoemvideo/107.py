5fb075f3b437c987e26b4f3330bc01cede193d39
Gustavo Martins <martins.gustavo@protonmail.com>
2022-09-20 15:53:51 +0200

    Adds price basic math script

---
+++ python/cursoemvideo/107.py
@@ -0,0 +1,8 @@
+from modules.currency import decrement, double, half, increment
+
+
+price = float(input('Digite o preço: '))
+print(f'A metade de R$ {price} é R$ {half(price)}.')
+print(f'O triplo de R$ {price} é R$ {double(price)}.')
+print(f'Aumentando 10%, temos {increment(price, 10)}.')
+print(f'Reduzindo 25%, temos {decrement(price, 25)}.')
