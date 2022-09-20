982f08da563d1c65c4249c173c1b1b3406b37143
Gustavo Martins <martins.gustavo@protonmail.com>
2022-09-20 15:49:11 +0200

    Adds currency module

---
+++ python/cursoemvideo/modules/currency.py
@@ -0,0 +1,22 @@
+def decrement(n, percent):
+	"""Returns n% decrement of n.
+	"""
+	return (n - (n * 1/percent))
+
+
+def double(n):
+	"""Returns double the value of n.
+	"""
+	return n * 2
+
+
+def half(n):
+	"""Returns half of a value n.
+	"""
+	return n / 2
+
+
+def increment(n, percent):
+	"""Returns n% increment of n.
+	"""
+	return (n + (n * 1/percent))
