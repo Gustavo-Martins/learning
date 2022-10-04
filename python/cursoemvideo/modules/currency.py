52fb8b7d75b679582532e6df1c611745c134bd6f
Gustavo Martins <martins.gustavo@protonmail.com>
2022-10-04 15:58:34 +0200

    Adds currency formatting.

--- python/cursoemvideo/modules/currency.py
+++ python/cursoemvideo/modules/currency.py
@@ -1,22 +1,28 @@
-def decrement(n, percent):
+def format(price, currency = 'R$'):
+	"""Returns price formatted as currency.
+	"""
+	return f'{currency} {price:>.2f}'.replace('.',',')
+
+
+def decrement(n = 0, percent = 0):
 	"""Returns n% decrement of n.
 	"""
 	return (n - (n * 1/percent))
 
 
-def double(n):
+def double(n = 0):
 	"""Returns double the value of n.
 	"""
 	return n * 2
 
 
-def half(n):
+def half(n = 0):
 	"""Returns half of a value n.
 	"""
 	return n / 2
 
 
-def increment(n, percent):
+def increment(n = 0, percent = 0):
 	"""Returns n% increment of n.
 	"""
 	return (n + (n * 1/percent))
