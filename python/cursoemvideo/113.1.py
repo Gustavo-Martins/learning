f945d8867bf4abe21a7793796af8eb69d0cf0fba
Gustavo Martins <martins.gustavo@protonmail.com>
2022-11-30 14:59:46 +0100

    Adds float input exception treatment

---
+++ python/cursoemvideo/113.1.py
@@ -0,0 +1,18 @@
+def read_float(msg):
+	"""Validates if input is a float."""
+	while True:
+		try:
+			n = float(input(msg))
+		except (TypeError, ValueError):
+			print('\033[0;31mERRO! Por favor, digite um número real válido.\033[m')
+			continue
+		except (KeyboardInterrupt):
+			print('\n\033[0;31mUsuário preferiu não informar um número.\033[m')
+			return 0
+		else:
+			return n
+			break
+
+
+number = read_float('Digite um número: ')
+print(f'Você digitou o número: {number:.2f}')
