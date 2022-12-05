fe37d2e8523c42971c7bd16e6c394338a8eb9441
Gustavo Martins <martins.gustavo@protonmail.com>
2022-12-05 10:44:49 +0100

    Adds Pudim site status check

---
+++ python/cursoemvideo/114.py
@@ -0,0 +1,12 @@
+# Checks Pudim.com.br status
+import urllib.request
+
+
+site = ("http://www.pudim.com.br")
+
+try:
+	code = urllib.request.urlopen(site).getcode()
+	if code == 200:
+		print('O site pudim está acessível no momento.')
+except:
+	print('O site Pudim não está acessível no momento.')
