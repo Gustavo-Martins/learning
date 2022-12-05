140db7ea9038e6eb466d7fa6907ad6480928f6a0
Gustavo Martins <martins.gustavo@protonmail.com>
2022-12-05 15:13:44 +0100

    Init interface package

--- python/cursoemvideo/115.py
+++ python/cursoemvideo/115.py
@@ -1,17 +1,20 @@
-flourish = ('-' * 35)
+from packages.interface.menu import flourish, menu_option
 
-print(flourish)
+
+
+print(flourish())
 print(f"{'MENU PRINCIPAL':^35}")
-print(flourish)
+print(flourish())
 print('1 - Ver pessoas cadastradas')
 print('2 - Cadastrar Novas Pessoas')
 print('3 - Sair do Sistema')
-print(flourish)
+print(flourish())
 
 while True:
 	try:
 		option = int(input('Digite uma opção: '))
-		print(option)
+		menu_option(option)
+
 		if option < 1 or option > 3:
 			print('Por favor, digite uma opção válida.')
 		elif option == 3:
---
+++ python/cursoemvideo/packages/interface/menu.py
@@ -0,0 +1,8 @@
+def flourish(size=35):
+	return '-' * size
+
+def menu_option(option):
+	print(f"Função menu_option recebeu: {option}")
+	print(flourish())
+	print(f"{'OPÇÃO' :^35}")
+	print(flourish())
